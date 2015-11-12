b40d62b6a681f0dbe8c5e57a59cb347df13340ec
Rebecca Bernstein <reb343@cornell.edu>
2015-11-12 18:13:20 -0500

    implemented create, push, and is_empty; untested

--- aQueue.ml
+++ aQueue.ml
@@ -1,13 +1,22 @@
 open Async.Std
 
-type 'a t
+type 'a t  = 'a list ref
 
+(** Create a new queue. *)
 let create () =
-  failwith "TODO"
+  ref []
 
-let push q x =
-  failwith "TODO"
+(** Add an element to the queue. *)
+let push (q: 'a t) (x: 'a) : unit =
+  q := List.append (!q) [x]
 
+(** Wait until an element becomes available, and then remove and return it. *)
 let pop  q =
   failwith "TODO"
+
+(** Return true if the queue is empty *)
+let is_empty (q: 'a t) : bool =
+  match !q with
+  | [] -> true
+  | _ -> false
 
