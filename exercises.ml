f5578144335c6c21749950a1aa2bbe7160e56402
Rebecca Bernstein <reb343@cornell.edu>
2015-11-11 22:47:18 -0500

    implemented job and both - tested both

--- exercises.ml
+++ exercises.ml
@@ -1,13 +1,26 @@
 open Async.Std
 
+(* It first prints "starting <name>" immediately when called.  After [t]
+ * seconds, it should print "finished <name>" and return its name*)
 let job name t =
-  failwith "TODO"
+  let _ = printf "starting %s\n" name in
+  after (Core.Std.sec t) >>= fun () ->
+  let _ = printf "finished %s\n" name in
+  return name
 
-let both d1 d2 =
-  failwith "TODO"
+(**[both d1 d2] becomes determined with value (a,b) after d1 becomes determined
+ * with value a and d2 becomes determined with value b.*)
+let both (d1:'a Deferred.t) (d2: 'b Deferred.t) : ('a * 'b) Deferred.t =
+  d1 >>= fun a ->
+  d2 >>= fun b ->
+  return (a,b)
 
-let fork d f1 f2 =
-  failwith "TODO"
+(**[fork d f g] runs [f] and [g] concurrently when d becomes determined.  The
+ * results returned by [f] and [g] are ignored.*)
+let fork (d:'a Deferred.t) (f1: 'a -> 'b Deferred.t) (f2:'a -> 'c Deferred.t):
+  unit =
+
+
 
 let parallel_map f l =
   failwith "TODO"
@@ -17,4 +30,9 @@ let sequential_map f l =
 
 let any ds =
   failwith "TODO"
+
+
+(* let _ = job "this" 5.0
+let _ = job "that" 2.0
+let _ = Scheduler.go () *)
 
--- tests.ml
+++ tests.ml
@@ -1,12 +1,23 @@
 (******************************************************************************)
 (** Unit tests for Exercises **************************************************)
 (******************************************************************************)
+open Async.Std
+open Exercises
+
+(*Taken from Lab 18*)
+let test_async_eq (d : 'a Deferred.t) (v : 'a) : bool =
+  Thread_safe.block_on_async (fun () -> d) = Core.Std.Result.Ok v
+
+let sec = Core.Std.sec
 
 (** Note: you do not need to write unit tests for job. *)
 
-TEST "both has unit tests" = failwith "TODO"
+TEST "both 1" = test_async_eq (both (return 1) (return 2)) (1, 2)
 
-TEST "fork has unit tests" = failwith "TODO"
+TEST "both 2" = test_async_eq (both (job "job1"  3.0) (job "job2"  2.0))
+  ("job1", "job2")
+
+(*TEST "fork has unit tests" = failwith "TODO"
 
 TEST "parallel_map has unit tests" = failwith "TODO"
 
@@ -24,6 +35,6 @@ TEST "push has unit tests" = failwith "TODO"
 
 TEST "pop has unit tests" = failwith "TODO"
 
-TEST "is_empty has unit tests" = failwith "TODO"
+TEST "is_empty has unit tests" = failwith "TODO"*)
 
 
