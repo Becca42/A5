4abf765306a4af80d9bc0ba890c897fd07c86fe8
Rebecca Bernstein <reb343@cornell.edu>
2015-11-12 17:50:40 -0500

    implemented parallel and sequention, one of which is wrong (not sure which); implemented any

--- exercises.ml
+++ exercises.ml
@@ -9,7 +9,9 @@ let job name t =
   return name
 
 (**[both d1 d2] becomes determined with value (a,b) after d1 becomes determined
- * with value a and d2 becomes determined with value b.*)
+ * with value a and d2 becomes determined with value b.
+
+ * taken from lecture 17 slides *)
 let both (d1:'a Deferred.t) (d2: 'b Deferred.t) : ('a * 'b) Deferred.t =
   d1 >>= fun a ->
   d2 >>= fun b ->
@@ -19,17 +21,53 @@ let both (d1:'a Deferred.t) (d2: 'b Deferred.t) : ('a * 'b) Deferred.t =
  * results returned by [f] and [g] are ignored.*)
 let fork (d:'a Deferred.t) (f1: 'a -> 'b Deferred.t) (f2:'a -> 'c Deferred.t):
   unit =
+  let _ = Deferred.bind d f1 in
+  let _ = Deferred.bind d f2 in
+  ()
 
+let fork_test (d:'a Deferred.t) (f1: 'a -> 'b Deferred.t) (f2:'a -> 'c Deferred.t) =
+  let _ = Deferred.bind d f1 in
+  let _ = Deferred.bind d f2 in
+  Deferred.peek d
 
+(**[parallel_map f xs] is similar to [List.map f xs]: it should apply [f] to
+ * every element of [xs], and return the list of results.
+ *
+ * It should call f on every element of xs immediately; it should not wait
+ * until one call completes before starting the next.*)
+let rec parallel_map (f:'a -> 'b Deferred.t) (l: 'a list) : 'b list Deferred.t =
+  match l with
+  | [] -> return []
+  | hd :: tl -> Deferred.bind (f hd) (fun x ->
+    Deferred.bind (parallel_map f tl) (fun y -> return (x :: y )))
 
-let parallel_map f l =
-  failwith "TODO"
+(**[sequential_map f xs] is similar to [List.map f xs]: it should apply [f] to
+ * every element of [xs], and return the list of results.
+ *
+ * It should wait for each call to f to return
+ * before proceeding to the next call.*)
+let sequential_map (f:'a -> 'b Deferred.t) (l:'a list) : 'b list Deferred.t =
+  match l with
+  | [] -> return []
+  | hd :: tl -> Deferred.bind (f hd) (fun x ->
+    Deferred.bind (parallel_map f tl) (fun y -> return (x :: y )))
 
-let sequential_map f l =
-  failwith "TODO"
+(**[any xs] becomes determined with value [v] if any of the elements of xs
+ * become determined with value [v].
 
-let any ds =
-  failwith "TODO"
+ * fill taken from lecture 17 slides*)
+let any (ds: 'a Deferred.t list) : 'a Deferred.t =
+    let result = Ivar.create () in
+    let fill = fun x ->
+      if Ivar.is_empty result
+      then Ivar.fill result x
+      else ()
+    in
+    let do_upon e =
+      upon e fill
+    in
+    let _ = List.map do_upon ds in
+    Ivar.read result
 
 
 (* let _ = job "this" 5.0
--- tests.ml
+++ tests.ml
@@ -14,12 +14,23 @@ let sec = Core.Std.sec
 
 TEST "both 1" = test_async_eq (both (return 1) (return 2)) (1, 2)
 
-TEST "both 2" = test_async_eq (both (job "job1"  3.0) (job "job2"  2.0))
+TEST "both 2" = test_async_eq (both (job "job1"  2.0) (job "job2"  1.0))
   ("job1", "job2")
 
-(*TEST "fork has unit tests" = failwith "TODO"
+TEST "fork returns unit" = fork (return 1) return return = ()
 
-TEST "parallel_map has unit tests" = failwith "TODO"
+let num = ref 2
+let for_test = fork (return (num := 3)) return return
+TEST "fork using side effects" = !num = 3
+
+let num = ref 2
+let for_test = fork (return num) (fun n -> return (n := !n + 1)) return
+let _ = return num
+TEST "fork using side effects 2" = !num = 3
+
+
+
+(*TEST "parallel_map has unit tests" = failwith "TODO"
 
 TEST "sequential_map has unit tests" = failwith "TODO"
 
